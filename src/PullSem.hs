bc7c463fb0c8db08a0e8f2f3cdafd581fa4fba2d
Joseph Cutler <jwc@seas.upenn.edu>
2024-09-27 13:32:20 -0400

    some nonsense

--- src/PullSem.hs
+++ src/PullSem.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module PullSem where
 
 import ElimTerm
@@ -72,3 +74,37 @@ denoteElimTerm e (SF x0 next_in) = SF (x0,e) next
 
 denoteElimTerm' :: ElimTerm -> Stream TaggedEvent -> Stream Event
 denoteElimTerm' a (S sf) = S (denoteElimTerm a sf)
+
+
+
+sem2 :: ElimTerm -> StreamFunc s TaggedEvent -> StreamFunc (s,RunState) Event
+sem2 (EUse (VarElim y) s) (SF i0 next) = SF (i0,STy s) $ \(i,STy s) ->
+    if isNull s then Done
+    else case next i of
+            -- TODO: We might need to distinguish an ``input done'' vs ``output done'' branch. The output done will carry the input state.
+            -- Maybe we can also break the step function apart into:
+            -- Step s s' a
+            Done -> Done
+            Skip i' -> Skip (i',STy s)
+            Yield (TEV x ev) i' -> if x == y then Yield ev (i', STy (deriv s ev)) else Skip (i',STy s)
+            -- if isNull s
+            -- then Done
+            -- else case nextFromElim x' c of
+                    -- Done -> Done
+                    -- Skip (x'',c') -> Skip (x'',EUse c' s)
+                    -- Yield ev (x'',c') -> Yield ev (x'', EUse c' (deriv s ev))
+sem2 EEpsR (SF x0 _) = SF (x0,SUnit) (const Done)
+sem2 (EIntR n) (SF x0 _) = SF (x0,SBool False) (\(x,SBool b) -> if b then Done else Yield (IntEv n) (x,SBool True))
+sem2 (ECatR e1 e2) s@(SF i0 _) =
+    let (SF (_,x0) next) = sem2 e1 s in
+    let (SF (_,y0) next') = sem2 e2 s in
+    SF (i0,SInL x0) $ \case
+                        (i,SInL x) -> case next (i,x) of
+                                        Yield ev (i',x') -> Yield (CatEvA ev) (i',SInL x')
+                                        Skip (i',x') -> Skip (i',SInL x')
+                                        Done -> Yield CatPunc (i,SInR y0)
+                        (i,SInR y) -> case next' (i,y) of
+                                        Done -> Done
+                                        Skip (i',y') -> Skip (i',SInR y')
+                                        Yield ev (i',y') -> Yield ev (i',SInR y')
+sem2 _ _ = undefined
